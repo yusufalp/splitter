50f59f331d09e6e425dbadd471b444efca6cbdc6
Yusuf Alp <alpyusuf007@gmail.com>
2021-08-14 11:01:18 -0700

    add the input and output components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 
-function App() {
+import React from 'react';
+import Inputs from './components/Inputs/Inputs';
+import Output from './components/Output/Output';
+
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Splitter</h1>
+        <Inputs />
+        <Output />
       </header>
     </div>
   );
