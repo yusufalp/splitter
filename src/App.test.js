e0ae5654600eca47a8af798f0dd7c7dae8a24e63
Yusuf Alp <alpyusuf007@gmail.com>
2021-08-17 06:18:59 -0700

    search for text of title

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('calculator for a tip', () => {
   render(<App />);
   const linkElement = screen.getByText(/Splitter/i);
   expect(linkElement).toBeInTheDocument();
