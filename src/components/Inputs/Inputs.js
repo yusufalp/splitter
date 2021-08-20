a0ccf0968320642582c88f77df2236d52220f414
Yusuf Alp <alpyusuf007@gmail.com>
2021-08-20 06:36:20 -0700

    add a form for inputs section

---
+++ src/components/Inputs/Inputs.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const Inputs = () => {
+  return (
+    <div>
+
+
+      Bill
+
+      Select Tip %
+      5%
+      10%
+      15%
+      25%
+      50%
+      Custom
+
+      Number of People
+    </div>
+  );
+}
+
+export default Inputs;
